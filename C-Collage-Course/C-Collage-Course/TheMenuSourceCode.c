a1b86271b38423b3afbbd3e755a29437554cfb28
NaorAhdut <naorahdut123@gmail.com>
2016-11-15 17:09:43 +0200

    Debugged and fixed the code.

--- C-Collage-Course/C-Collage-Course/TheMenuSourceCode.c
+++ C-Collage-Course/C-Collage-Course/TheMenuSourceCode.c
@@ -6,12 +6,12 @@
 
 int main()
 {
-	const int CHIPS = 10, STEAK = 60, SALAD = 15, TUNA = 50, SALMON = 70;
+	const int CHIPS = 10, STEAK = 60, SALAD = 15, TUNA = 50, SALMON = 70, NUM_OF_DISHES = 5;
 	char *theMenu[5] = { "TUNA" , "CHIPS" , "SALMON" , "STEAK" , "SALAD" };
 	int userChoose;
 	int userSavedDishes[5];
 	int i;
-	for (i = 0; i < sizeof(userSavedDishes); i++)
+	for (i = 0; i < NUM_OF_DISHES; i++)
 	{
 		userSavedDishes[i] = 0;
 	}
@@ -21,15 +21,14 @@ int main()
 		printf("Please Choose The Dishes:\n");
 		printf("1.TUNA, 2.CHIPS , 3.SALMON , 4.STEAK , 5.SALAD\n");
 		scanf("%d", &userChoose);
-		if (userChoose < 0 || sizeof(userSavedDishes) < userChoose)
+		if (userChoose < 0 || NUM_OF_DISHES < userChoose)
 		{
 			continue;
 		}
 		userSavedDishes[userChoose - 1] += 1;
 
 		printf("Your Orders Sire:");
-		for (
-			i = 0; i < sizeof(userSavedDishes); i++)
+		for (i = 0; i < NUM_OF_DISHES; i++)
 		{
 			if (userSavedDishes != 0)
 			{
@@ -37,7 +36,7 @@ int main()
 			}
 		}
 		char anotherDishYesNo[] = "No";
-		printf("Would You Like Another Dish ? (Yes / No )");
+		printf("\nWould You Like Another Dish ? (Yes / No )");
 		scanf("%s", anotherDishYesNo);
 		if (strcmp(anotherDishYesNo, "Yes") == 0)
 		{
