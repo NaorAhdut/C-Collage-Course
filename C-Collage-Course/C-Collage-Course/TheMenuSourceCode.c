7fa8e3e100eddde4f4a22ed9f58840b8f4af6282
NaorAhdut <naorahdut123@gmail.com>
2016-11-16 13:55:05 +0200

    Added Login Function with user and password. sorted the main function and splited the rest of the code to a clean look.

--- C-Collage-Course/C-Collage-Course/TheMenuSourceCode.c
+++ C-Collage-Course/C-Collage-Course/TheMenuSourceCode.c
@@ -7,11 +7,54 @@
 
 int main()
 {
+	const int numberOfusers = 3;
+	char *users[3][2] = { { "", "" }, { "", "" }, { "Naor", "123456" } };
+	int isValid = login();
+	if (isValid == 1) 
+	{
+		menu();
+	}
+	else
+	{
+		createUser(users, numberOfusers);
+	}
+	return 0;
+}
+
+void createUser(char users, int numberOfusers)
+{
+	int i;
+	for (i = 0; i < numberOfusers; i++)
+	{
+
+	}
+
+}
+
+
+int login()
+{
+	printf("Are You A Memeber Of BlackPaper? ( Yes / No )\n");
+	char userExist[] = "Yes";
+	scanf("%s", userExist);
+	if (strcmp(userExist, "Yes") == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int menu()
+{
 	const int CHIPS = 10, STEAK = 60, SALAD = 15, TUNA = 50, SALMON = 70, NUM_OF_DISHES = 5;
 	char *theMenu[5] = { "TUNA" , "CHIPS" , "SALMON" , "STEAK" , "SALAD" };
 	int userChoose;
 	int userSavedDishes[5];
 	int i;
+	
 	for (i = 0; i < NUM_OF_DISHES; i++)
 	{
 		userSavedDishes[i] = 0;
@@ -45,7 +88,5 @@ int main()
 		}
 		break;
 	}
-
-
-	return 0;
+	return userSavedDishes;
 }
